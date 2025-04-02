9674f00255d7ec63880ad5488144f1a3c13d5d8a
SarkadiKristofBence <skrisiman@gmail.com>
2025-04-02 11:23:50 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import PetForm from './components/PetForm'
+import FoodList from './components/FoodList'
 
 function App() {
   return (
     <div style={{ padding: '2rem', fontFamily: 'Arial, sans-serif' }}>
       <h1>PawPlan 🐾</h1>
       <PetForm />
+      <FoodList />
     </div>
   )
 }
